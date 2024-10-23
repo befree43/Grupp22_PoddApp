ace4e93c620e14595f96d9b1e987e8dece60d416
befree43 <marina.grane@gmail.com>
2024-10-23 14:15:16 +0200

    Lagt till klasser

---
+++ Models/Avsnitt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Avsnitt
+    {
+        private string? titel;
+        private string? beskrivning;
+        private string? id;
+
+        public Avsnitt(string? titel, string? beskrivning, string? id)
+        {
+            this.titel = titel;
+            this.beskrivning = beskrivning;
+            this.id = id;
+        }
+    }
+}
---
+++ Models/Podcasts.cs
@@ -0,0 +1,16 @@
+﻿namespace Models
+{
+    public class Podcasts
+    {
+        private string? titel;
+        private string? kategori;
+        private string? url;
+
+        public Podcasts (string? titel, string? kategori, string? url)
+        {
+            this.titel = titel;
+            this.kategori = kategori;
+            this.url = url;
+        }
+    }
+}
