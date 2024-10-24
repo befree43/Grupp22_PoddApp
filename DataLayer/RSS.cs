179051156f2162571a99d7950fac309879dedc34
befree43 <marina.grane@gmail.com>
2024-10-24 14:57:43 +0200

    Uppdaterat klassen RSS, inga felmeddelanden.

--- DataLayer/RSS.cs
+++ DataLayer/RSS.cs
@@ -1,22 +1,44 @@
 ﻿using System.ServiceModel.Syndication;
+using System.Xml;
 using DataAccessLayer.Repository;
 using Models;
+
 
 namespace DataAccessLayer
 {
     public class RSS
     {
-        public void HämtaPodcastFrånRss(string url);
-        SyndicationFeed podcastFlode = SyndicationFeed.Load(minXMLlasare);
+        private PodRepository podRepository;
 
-        foreach (SyndicationItem item in podcastFlode.Items)
-            {
-            Podcast enPodcast = new Podcast
-            {
-                Url = item.Id.ToString(),
-                Titel = item.Title.Text
-            };
+        public RSS()
+        {
+            podRepository = new PodRepository(); // Skapa en instans av PodRepository
+        }
 
-        
+        public string? Titel { get; private set; }
+
+        public List<Podcast> HämtaPodcastFrånRss(string url)
+        {
+            List<Podcast> podcasts = new List<Podcast>();
+
+            using (XmlReader xmlReader = XmlReader.Create(url)) // Skapa en XML-läsare för att läsa RSS
+            {
+                SyndicationFeed podcastFlode = SyndicationFeed.Load(xmlReader);
+
+                foreach (SyndicationItem item in podcastFlode.Items)
+                {
+                    Podcast enPodcast = new Podcast()
+                    {
+                        Url = item.Id.ToString(),
+                        Titel = item.Title.Text
+                    };
+
+                    podcasts.Add(enPodcast); // Lägg till varje podcast i listan
+                }
+            }
+
+            return podcasts; // Returnera listan med podcasts
+        }
     }
 }
+
